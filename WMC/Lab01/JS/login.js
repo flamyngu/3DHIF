0781230c64bfd49f880f4354217e4c987188d2af
Martin Stanev <159732128+flamyngu@users.noreply.github.com>
2025-09-07 17:25:33 +0400

    login page finished

---
+++ WMC/Lab01/JS/login.js
@@ -0,0 +1,43 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const errorMsg = document.getElementById("errorMsg");
+
+  errorMsg.textContent = "";
+
+  if (!username || !password) {
+    errorMsg.textContent = "Bitte alle Felder ausfüllen.";
+    return;
+  }
+
+  if (password.length < 4) {
+    errorMsg.textContent = "Passwort muss mindestens 4 Zeichen lang sein.";
+    return;
+  }
+
+  let users = JSON.parse(localStorage.getItem("users")) || {};
+
+  if (users[username]) {
+    if (users[username] === password) {
+      alert("Login erfolgreich! Willkommen zurück, " + username);
+    } else {
+      errorMsg.textContent = "Falsches Passwort.";
+    }
+  } else {
+    users[username] = password;
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Neuer Account erstellt. Willkommen, " + username);
+  }
+});
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    togglePassword.src = "../icons/einblenden.png"; 
+  } else {
+    passwordInput.type = "password";
+    togglePassword.src = "../icons/ausblenden.png"; 
+  }
+});
